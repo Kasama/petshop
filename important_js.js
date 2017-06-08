065b2817e28e472715d1be7d074c696174f6f300
Garunts <gasbicalho@gmail.com>
2017-06-08 01:13:59 -0300

    Begun login system

--- important_js.js
+++ important_js.js
@@ -2,3 +2,7 @@
 function changePage(that, tag_to_replace, additional_data = null){
 	that.fire("iron-signal", {name: "change-signal", tag: tag_to_replace, data: additional_data});
 }
+
+function sendMessage(that, name, additional_data = null){
+	that.fire("iron-signal", {name: name, data: additional_data});
+}
