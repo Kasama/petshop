1a10e34bfe8cce03935756a32b87701e089d68d5
Garunts <gasbicalho@gmail.com>
2017-06-08 02:46:13 -0300

    Did stuff

--- important_js.js
+++ important_js.js
@@ -6,3 +6,13 @@ function changePage(that, tag_to_replace, additional_data = null){
 function sendMessage(that, name, additional_data = null){
 	that.fire("iron-signal", {name: name, data: additional_data});
 }
+
+function addToCart(product_id, quantity){
+	cart = Cookies.get("cart");
+	if(cart[product_id]){
+		cart[product_id] += quantity;
+	}else{
+		cart[product_id] = quantity;
+	}
+	Cookies.set("cart", cart);
+}
